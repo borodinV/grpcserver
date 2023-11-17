cd054968ec26b94814f0b839cd0ccb29b2248d86
borodinV <foggy.pale@mail.ru>
2023-11-18 00:21:18 +0300

    grpc

--- grpc/server/migrations/00002_alter_tables.sql
+++
@@ -1,8 +0,0 @@
--- +goose Up
--- SQL in this section is executed when the migration is applied.
-ALTER TABLE library
-ADD created_at date,
-ADD updated_at date;
-
--- +goose Down
--- SQL in this section is executed when the migration is rolled back.
